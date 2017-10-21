24fd0d82ec4c7d8f336aa5abfa116b356b4346d2
齋藤健悟 <k.saitoh0526@gmail.com>
2017-10-21 19:20:34 +0900

    文字列の色を順番に変更するようにした

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloWorldLabel.textColor = UIColor.black
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchUpInsideTestButton(_ sender: UIButton) {
+        if count == 0 {
+            helloWorldLabel.textColor = .blue
+            count = 1
+        } else {
+            if let helloText = helloWorldLabel.text,
+                let helloCount = helloWorldLabel.text?.count {
+                
+                let attrText = NSMutableAttributedString(string: helloText)
+                attrText.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.red, range: NSMakeRange(0, count))
+                helloWorldLabel.attributedText = attrText
+                count += 1
+                if count > helloCount {
+                    count = 0
+                }
+            }
+        }
+    }
 }
 
